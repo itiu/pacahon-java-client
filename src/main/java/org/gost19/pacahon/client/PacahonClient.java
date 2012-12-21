b6b48d84c98427a02f63d2403ccea2b6cf1ef34e
Itiu <ValeriyBushenev@gmail.com>
2012-12-21 16:50:27 +0400

    ++

--- src/main/java/org/gost19/pacahon/client/PacahonClient.java
+++ src/main/java/org/gost19/pacahon/client/PacahonClient.java
@@ -194,7 +194,7 @@ public class PacahonClient
 		return false;
 	}
 
-	public static String command_as_string(String ticket, String command, JSONObject data, String from)
+	public synchronized String command_as_string(String ticket, String command, JSONObject data, String from)
 	{
 		UUID msg_uuid = UUID.randomUUID();
 
